1dcea487dac73c9ef73bd94a253b0c2f6216798a
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-09-07 20:07:31 +0000

    Add maps and marker to interest file

---
+++ assets/js/maps.js
@@ -0,0 +1,42 @@
+//var APIkey = 'AIzaSyDIQx9iSJ7LxbmE-Aja3-_9ctatLn7WdDw';
+
+function initMap() {
+    var location = {lat: 46.619261, lng: -33.13766}
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 3,
+        center: location
+    });
+    // Places one marker
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
+};
+
+/*function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
+*/
