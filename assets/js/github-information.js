ff3f75ea09fe80eeaf275394cc0e01e45257e968
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-09-07 20:47:31 +0000

    Link jss files to html pages

---
+++ assets/js/github-information.js
@@ -0,0 +1,10 @@
+function fetchGithubInformation(event) {
+    var username = $('#gh-username').val();
+    if (!username) {
+        $('#gh-user-data').html(`<h2>Please enter a Github username</h2>`);
+        return;
+    }
+    $('#gh-user-data').html(
+        `<div id="loader"><img src="assets/css/loader.gif" alt="loading..."></div>`
+    )
+}
