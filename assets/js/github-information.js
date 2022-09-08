e6c02ed3d26b28ffeed2474188f773337fe2b64b
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-09-08 21:01:33 +0000

    Add fetch Github Info function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,10 +1,35 @@
-function fetchGithubInformation(event) {
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>
+            </span>
+        </h2>`
+}
+
+
+function fetchGitHubInformation(event) {
+
     var username = $('#gh-username').val();
     if (!username) {
         $('#gh-user-data').html(`<h2>Please enter a Github username</h2>`);
         return;
     }
-    $('#gh-user-data').html(
-        `<div id="loader"><img src="assets/css/loader.gif" alt="loading..."></div>`
+    $('#gh-user-data').html(`<div id="loader"><img src="assets/css/loader.gif" alt="Loading..."></div>`)
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function (response) {
+            var userData = response;
+            $('#gh-user-data').html(userInformationHTML(userData));
+        }, function (errorResponse) {
+            if (errorResponse.status === 404) {
+                $('#gh-user-data').html(`<h2>No info found for user ${username}</h2>`)
+            } else {
+                console.log(errorResponse);
+                $('#gh-user-data').html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        }
     )
 }
