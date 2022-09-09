249ce8bb20f263e5af7654b20a9bb9c516cae452
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-09-09 09:08:48 +0000

    Render user information from github

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -4,7 +4,15 @@ function userInformationHTML(user) {
             <span class="small-name">
                 (@<a href="${user.html_url}" target="_blank">${user.login}</a>
             </span>
-        </h2>`
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}">                
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`
 }
 
 
